6f87369890ce4b9f2e65909b665e678df5e6b2ab
Fabrizio Bagalà <fabrizio.bagala@outlook.com>
2020-09-21 18:24:33 +0200

    Add PokemonController.cs

---
+++ src/Pokemon/Controllers/PokemonController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pokemon.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        public PokemonController()
+        {
+        }
+
+        // GET pokemon
+        [HttpGet]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET pokemon/{name}
+        [HttpGet("{name}")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status503ServiceUnavailable)]
+        public async Task<ActionResult> Get(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
