5be2a60ebb0e756136db3fd61e7df28769ca99b1
Fabrizio Bagalà <fabrizio.bagala@outlook.com>
2020-09-21 18:16:39 +0200

    Prepare TDD development

---
+++ src/Pokemon.Tests/PokemonControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Pokemon.Tests
+{
+    public class PokemonControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Get_ReturnsOkObjectResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Get_WithName_ReturnsOkObjectResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Get_WithNonExistingName_ReturnsNotFoundObjectResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Get_WithFunTranslationsApiNotAvailable_ReturnsServiceUnavailable()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
